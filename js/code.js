03652fb615c9490c4719f1e60fb0922af2ad9f81
isakjensen <isakludvigjensen@gmail.com>
2022-01-19 10:45:45 +0100

    Dynamic table loop using alphinejs

--- js/code.js
+++ js/code.js
@@ -10,87 +10,85 @@ class Process {
 }
 
 class FIFO {
-    constructor() {
-        this.queueLength = 0;
-        this.queue = null;
-    }
+  constructor() {
+    this.queueLength = 0;
+    this.queue = null;
+  }
 
-    add(process) {
+  add(process) {
     let newProcess = new Process(process[0], process[1], process[2]);
     if (!this.queue) {
-        this.queue = [newProcess];
-        this.queueLength = 1;
+      this.queue = [newProcess];
+      this.queueLength = 1;
     } else {
-        this.queue.push(newProcess);
-        this.queueLength++;
+      this.queue.push(newProcess);
+      this.queueLength++;
     }
-    }
+  }
 
-    remove() {
-        if (this.queueLength == 1) {
-            this.queue = null;
-            this.queueLength = 0
-        }
-        else {
-            this.queue.shift();
-            this.queueLength--;
-        }
+  remove() {
+    if (this.queueLength == 1) {
+      this.queue = null;
+      this.queueLength = 0;
+    } else {
+      this.queue.shift();
+      this.queueLength--;
     }
-    
-    work(ms) {
-        this.queue[0].execTime -= ms;
-        if (this.queue[0].execTime <= 0) {
-            if (this.queueLength > 1) {
-                let temp = this.queue[0].execTime;
-                this.remove();
-                this.queue[0].execTime -= temp;
-            }
-            else {
-                this.remove();
-            }
-        }
+  }
+
+  work(ms) {
+    this.queue[0].execTime -= ms;
+    if (this.queue[0].execTime <= 0) {
+      if (this.queueLength > 1) {
+        let temp = this.queue[0].execTime;
+        this.remove();
+        this.queue[0].execTime -= temp;
+      } else {
+        this.remove();
+      }
     }
+    console.log('Work done:', this.queue);
+  }
 }
 
 class SHARE {
-    constructor() {
-        this.queueLength = 0;
-        this.queue = null;
+  constructor() {
+    this.queueLength = 0;
+    this.queue = null;
+  }
+
+  add(process) {
+    let newProcess = new Process(process[0], process[1], process[2]);
+    let nextProcess = this.queue;
+    if (!this.queue) {
+      this.queue = newProcess;
+    } else {
+      while (nextProcess.preceding) {
+        nextProcess = nextProcess.preceding;
+      }
+      nextProcess.preceding = newProcess;
     }
-    
-    add(process) {
-        let newProcess = new Process(process[0],process[1],process[2]);
-        let nextProcess = this.queue;
-        if (!this.queue) {
-            this.queue = newProcess;
-        }
-        else {
-            while (nextProcess.preceding) {
-                nextProcess = nextProcess.preceding;
-            }
-            nextProcess.preceding = newProcess;
-        }
+  }
+
+  remove(process) {
+    let nextProcess = this.queue;
+    while (nextProcess.preceding) {
+      if (nextProcess == process) {
+        break;
+      }
+      nextProcess = nextProcess.preceding;
     }
-    
-    remove(process) {
-        let nextProcess = this.queue;
-        while (nextProcess.preceding) {
-            if (nextProcess == process) {
-                break;
-            }
-            nextProcess = nextProcess.preceding;
-        }
+  }
+
+  work(ms) {
+    let times = Math.floor(100 / this.queueLength);
+    if (this.queueLength >= 100) {
+      times = 1;
     }
-    
-    work(ms) {
-        let times = Math.floor(100/this.queueLength);
-        if (this.queueLength >= 100) {
-            times = 1;
-        }
-        if (this.queue[0].execTime <= 0) {
-            this.remove();
-        }
+    if (this.queue[0].execTime <= 0) {
+      this.remove();
     }
+  }
 }
 
 class PRIO {
@@ -110,7 +108,7 @@ class PRIO {
 
 let processInterval = 100;
 let Stop = false;
-const clock = setInterval(scheduler,processInterval);
+const clock = setInterval(scheduler, processInterval);
 
 // All buttons
 const ProcessButton = document.getElementById("processBtn");
@@ -136,7 +134,7 @@ ClearButton.addEventListener("click", function () {
 const clearTasksButton = document.getElementById("clearTasks");
 
 clearTasksButton.addEventListener("click", function () {
-    ClearTasks(CPU1);
+  ClearTasks(CPU1);
 });
 
 LoadButton.addEventListener("click", function () {
@@ -151,46 +149,44 @@ LoadButton.addEventListener("click", function () {
     FileInput.value = null;
   };
 });
-StartStopButton.addEventListener("click", function(){
-    Stop = !Stop;
-    if (Stop == true) {
-        StartStopButton.innerHTML = "Start";
-        StartStopButton.style.backgroundColor = "rgb(239, 239, 239)";
-        console.log("stoped");
-    }
-    else if (Stop == false) {
-        StartStopButton.innerHTML = "Stop";
-        StartStopButton.style.backgroundColor = "red";
-        console.log("started");
-    }
+StartStopButton.addEventListener("click", function () {
+  Stop = !Stop;
+  if (Stop == true) {
+    StartStopButton.innerHTML = "Start";
+    StartStopButton.style.backgroundColor = "rgb(239, 239, 239)";
+    console.log("stoped");
+  } else if (Stop == false) {
+    StartStopButton.innerHTML = "Stop";
+    StartStopButton.style.backgroundColor = "red";
+    console.log("started");
+  }
 });
 
-
 function dispatcher() {
-    let currentBatch = textParser();
-    console.log(currentBatch);
-    for (process in currentBatch) {
-        CPU1.add(currentBatch[process]);
-        CPU2.add(currentBatch[process]);
-        CPU3.add(currentBatch[process]);
-    }
-    Input.value = "";
+  let currentBatch = textParser();
+  console.log(currentBatch);
+  for (process in currentBatch) {
+    CPU1.add(currentBatch[process]);
+    CPU2.add(currentBatch[process]);
+    CPU3.add(currentBatch[process]);
+  }
+  Input.value = "";
 
   DrawAllTasks(currentBatch);
 }
 
 function scheduler() {
-    if (Stop === false) {
-        if (CPU1.queue) {
-            CPU1.work(processInterval);
-        }
-        // if (CPU2.queue) {
-        //     CPU2.work(processInterval);
-        // }
-        if (CPU3.queue) {
-            CPU3.work(processInterval);
-        }
+  if (Stop === false) {
+    if (CPU1.queue) {
+      CPU1.work(processInterval);
     }
+    // if (CPU2.queue) {
+    //     CPU2.work(processInterval);
+    // }
+    if (CPU3.queue) {
+      CPU3.work(processInterval);
+    }
+  }
 }
 
 function textParser() {
@@ -276,9 +272,16 @@ function ClearTasks(cpu) {
     el.removeChild(el.lastElementChild);
   }
 
-  console.log('This is the current queue:', cpu.queue);
+  console.log("This is the current queue:", cpu.queue);
 
+  // Reset the queue & it's length
+  cpu.queueLength = 0;
   cpu.queue = null;
 
-  console.log('The queue is now empty:', cpu.queue);
+  console.log("The queue is now empty:", cpu.queue);
+}
+
+function FormatCPUTitle(string)
+{
+  return (string.slice(0, -1) + ' ' + string.slice(-1)).toUpperCase();
 }
